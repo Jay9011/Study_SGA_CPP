5710b7115d82cad6a8a2dcf320f8a9ed89f4a993
Jay9011 <whdtjq5904@gmail.com>
2022-06-27 14:02:55 +0900

    SolarSystem, Planet

--- 32_DirectX/DX9_Framework/Scene/Scenes/SolarSystemScene.cpp
+++ 32_DirectX/DX9_Framework/Scene/Scenes/SolarSystemScene.cpp
@@ -3,7 +3,7 @@
 
 SolarSystemScene::SolarSystemScene()
 {
-	sun = new Planet(0xFFAA3300, 100, .01f, .01f, 12);
+	sun = new Planet(0xFFAA3300, 0xFFAAAA00, 100.f, .01f, .01f, 12);
 	sun->pos = { WIN_CENTER_X, WIN_CENTER_Y };
 }
 
